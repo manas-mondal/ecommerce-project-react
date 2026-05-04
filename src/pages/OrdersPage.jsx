4589cc6af276e143a80470aa4b11062687bada42
Manas Mondal <manasmondal035@gmail.com>
2026-05-04 21:37:23 +0530

    refactor: replace remaining anchor tags with Link components

--- src/pages/OrdersPage.jsx
+++ src/pages/OrdersPage.jsx
@@ -1,4 +1,5 @@
 import { Header } from "../components/Header";
+import { Link } from "react-router";
 import "./OrdersPage.css";
 
 export function OrdersPage() {
@@ -55,11 +56,11 @@ export function OrdersPage() {
               </div>
 
               <div className="product-actions">
-                <a href="/tracking">
+                <Link to="/tracking">
                   <button className="track-package-button button-secondary">
                     Track package
                   </button>
-                </a>
+                </Link>
               </div>
 
               <div className="product-image-container">
@@ -84,11 +85,11 @@ export function OrdersPage() {
               </div>
 
               <div className="product-actions">
-                <a href="/tracking">
+                <Link to="/tracking">
                   <button className="track-package-button button-secondary">
                     Track package
                   </button>
-                </a>
+                </Link>
               </div>
             </div>
           </div>
@@ -133,11 +134,11 @@ export function OrdersPage() {
               </div>
 
               <div className="product-actions">
-                <a href="/tracking">
+                <Link to="/tracking">
                   <button className="track-package-button button-secondary">
                     Track package
                   </button>
-                </a>
+                </Link>
               </div>
             </div>
           </div>
