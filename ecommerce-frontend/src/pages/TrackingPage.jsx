2c0870cd2e2d106b62cbd62e5774dd82051417b8
Manas Mondal <manasmondal035@gmail.com>
2026-05-18 13:17:26 +0530

    feat: load tracking page data from backend API

--- ecommerce-frontend/src/pages/TrackingPage.jsx
+++ ecommerce-frontend/src/pages/TrackingPage.jsx
@@ -1,3 +1,6 @@
+import axios from "axios";
+import dayjs from "dayjs";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import { Header } from "../components/Header";
 import { Link } from "react-router";
@@ -5,6 +8,25 @@ import "./TrackingPage.css";
 
 export function TrackingPage({ cart }) {
   const { orderId, productId } = useParams();
+
+  const [order, setOrder] = useState(null);
+  useEffect(() => {
+    const fetchTrackingData = async () => {
+      const response = await axios.get(
+        `/api/orders/${orderId}?expand=products`,
+      );
+      setOrder(response.data);
+    };
+    fetchTrackingData();
+  }, [orderId]);
+
+  if (!order) {
+    return null;
+  }
+
+  const orderProduct = order.products.find((orderProduct) => {
+    return orderProduct.productId === productId;
+  });
   return (
     <>
       <title>Tracking</title>
@@ -18,18 +40,16 @@ export function TrackingPage({ cart }) {
             View all orders
           </Link>
 
-          <div className="delivery-date">Arriving on Monday, June 13</div>
-
-          <div className="product-info">
-            Black and Gray Athletic Cotton Socks - 6 Pairs
+          <div className="delivery-date">
+            Arriving on{" "}
+            {dayjs(orderProduct.estimatedDeliveryTimeMs).format("dddd, MMMM D")}
           </div>
 
-          <div className="product-info">Quantity: 1</div>
+          <div className="product-info">{orderProduct.product.name}</div>
 
-          <img
-            className="product-image"
-            src="images/products/athletic-cotton-socks-6-pairs.jpg"
-          />
+          <div className="product-info">Quantity: {orderProduct.quantity}</div>
+
+          <img className="product-image" src={orderProduct.product.image} />
 
           <div className="progress-labels-container">
             <div className="progress-label">Preparing</div>
