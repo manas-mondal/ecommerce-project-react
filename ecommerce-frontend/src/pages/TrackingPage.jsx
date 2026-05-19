64856f858d2561a90d48a88b5e3add9271493a67
Manas Mondal <manasmondal035@gmail.com>
2026-05-19 14:01:08 +0530

    feat: calculate and display delivery progress in TrackingPage

--- ecommerce-frontend/src/pages/TrackingPage.jsx
+++ ecommerce-frontend/src/pages/TrackingPage.jsx
@@ -27,6 +27,14 @@ export function TrackingPage({ cart }) {
   const orderProduct = order.products.find((orderProduct) => {
     return orderProduct.productId === productId;
   });
+
+  const totalDeliveryTimeMs =
+    orderProduct.estimatedDeliveryTimeMs - order.orderTimeMs;
+  const timePassedMs = dayjs().valueOf() - order.orderTimeMs;
+  let deliveryPercent = (timePassedMs / totalDeliveryTimeMs) * 100;
+  if (deliveryPercent > 100) {
+    deliveryPercent = 100;
+  }
   return (
     <>
       <title>Tracking</title>
@@ -58,7 +66,10 @@ export function TrackingPage({ cart }) {
           </div>
 
           <div className="progress-bar-container">
-            <div className="progress-bar"></div>
+            <div
+              className="progress-bar"
+              style={{ width: `${deliveryPercent}%` }}
+            ></div>
           </div>
         </div>
       </div>
